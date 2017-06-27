93c709fec6921c92299c7d957332b5dac449e65c
Dams68540 <tabetdamien@free.fr>
2017-06-27 09:31:14 +0200

    modification Admin.php

--- Admin.php
+++ Admin.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=Blog', 'root', 'toor');
+$bdd = new PDO('mysql:host=localhost:8889;dbname=Blog', 'root', 'root');
 session_start();
 if (isset($_SESSION['id']))
 {
